a316c48b18eaeb2fc7b83e65e3493e059ffedc6b
Vyacheslav Zaicev <v.zaicevs@gmail.com>
2020-11-22 01:50:29 +0200

    skaffold script added

--- server/index.js
+++ server/index.js
@@ -43,6 +43,7 @@ app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
+
 });
 
 app.get('/values/current', async (req, res) => {
